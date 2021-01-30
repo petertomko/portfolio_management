b28c88e7913e07e6be573e6b0e906c439742d201
Peter Tomko <peter.tomko@aaaauto.cz>
2021-01-30 10:24:44 +0100

    test module import

---
+++ module import.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import pystan
+import plotly
+import matplotlib
+import fbprophet
+import sklearn
+import tensorflow as tf
+import keras
+import torch
+import theano
