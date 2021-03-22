2ba641746e2ae2ad6fc965287da721d17ac58a68
PeterTomko <peter.tomko@kindredgroup.cz>
2021-03-22 22:19:29 +0100

    add funcionality to run code on working PC

--- 0 data download/run stock price download.R
+++ 0 data download/run stock price download.R
@@ -1,5 +1,16 @@
-rstudioapi::jobRunScript(
-  path = "./0 data download/stock price download.R", 
-  name = "Download Price Data", 
-  workingDir = "C:/Users/Peter/Desktop/ds_projects/portfolio_management"
-)
+if(as.character(Sys.info()["nodename"]) == "ND0151MB"){
+  
+  rstudioapi::jobRunScript(
+    path = "./0 data download/stock price download.R", 
+    name = "Download Price Data", 
+    workingDir = "C:/Users/peter.tomko/OneDrive - Publicis Groupe/Desktop/ds_projects/portfolio_management/"
+  )
+  
+}else{
+  
+  rstudioapi::jobRunScript(
+    path = "./0 data download/stock price download.R", 
+    name = "Download Price Data", 
+    workingDir = "C:/Users/Peter/Desktop/ds_projects/portfolio_management"
+  )
+}
