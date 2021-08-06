89e4956db2e9392cb8e6a4a27709b1d2858da60b
PeterTomko <peter.tomko@kindredgroup.cz>
2021-08-06 12:03:47 +0200

    excluding files and moving the code to another parts

--- 0 data download/run stock price download.R
+++
@@ -1,5 +0,0 @@
-rstudioapi::jobRunScript(
-  path = "./0 data download/stock price download.R", 
-  name = "Download Price Data", 
-  workingDir = paste(home_dir, "/portfolio_management/", sep = "")
-)
--- runDataPreparation.R
+++
@@ -1,7 +0,0 @@
-rstudioapi::jobRunScript(path = "0 data download/stock price download.R", 
-                         name = "Download Daily Stock Prices", 
-                         workingDir = "C:/Users/peter.tomko/OneDrive - Publicis Groupe/Desktop/ds_projects/portfolio_management")
-
-rstudioapi::jobRunScript(path = "1 variable preparation/technical indicators.R", 
-                         name = "Compute Technical Indicators", 
-                         workingDir = "C:/Users/peter.tomko/OneDrive - Publicis Groupe/Desktop/ds_projects/portfolio_management")
