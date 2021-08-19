4a1ef6ea6b90a5fc2d645c03a136697cf6119d9b
PeterTomko <peter.tomko@kindredgroup.cz>
2021-08-19 15:06:33 +0200

    rendering Rmd documents for scheduling job within app

---
+++ adhoc analysis/runFiles/ADF.R
@@ -0,0 +1,3 @@
+# - Run ADF Test
+rmarkdown::render(input = "C:/Users/peter.tomko/OneDrive - Publicis Groupe/Desktop/ds_projects/portfolio_management/adhoc analysis/ADF Test.Rmd", 
+                  output_file = "C:/Users/peter.tomko/OneDrive - Publicis Groupe/Desktop/ds_projects/portfolio_management/adhoc analysis/ADF-Test.html")
---
+++ adhoc analysis/runFiles/Hurst.R
@@ -0,0 +1,3 @@
+# - Run Hurst Exponent Computation
+rmarkdown::render(input = "C:/Users/peter.tomko/OneDrive - Publicis Groupe/Desktop/ds_projects/portfolio_management/adhoc analysis/Stationarity Test using Hurst Exponent.Rmd", 
+                  output_file = "C:/Users/peter.tomko/OneDrive - Publicis Groupe/Desktop/ds_projects/portfolio_management/adhoc analysis/Stationarity-Test-using-Hurst-Exponent.html")
