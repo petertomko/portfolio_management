a40645dcf196e7c8f027aeb60b1b2e44ad43234d
PeterTomko <peter.tomko@kindredgroup.cz>
2021-08-19 15:05:12 +0200

    remove unneccessary files

--- runDataPreparationSequence.R
+++
@@ -1,3 +0,0 @@
-rstudioapi::jobRunScript(path = "./Data Preparation Sequence.R", 
-                         name = "Data Preparation Sequence - Download, Compute Technical Indicators, Store in Board", 
-                         workingDir = "C:/Users/peter.tomko/OneDrive - Publicis Groupe/Desktop/ds_projects/portfolio_management")
